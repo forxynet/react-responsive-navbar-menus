d200679331f447c8ad6a07a8c88e89841285cb1e
hakan.basturknewinn.com <forxynet@gmail.com>
2025-01-09 18:31:57 +0300

    updated navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,12 @@
 import { Link } from 'react-router-dom';
-import { useRef } from 'react';
+import { useState } from 'react';
 
 export default function Navbar() {
+  const [isShowCreate, setIsShowCreate] = useState();
+  const [isShowTodoList, setIsShowTodoList] = useState();
+
+
+
   const toggleButton = document.getElementById('toggle-btn')
   const sidebar = document.getElementById('sidebar')
 
@@ -12,28 +17,10 @@ export default function Navbar() {
     closeAllSubMenus()
   }
 
-  function toggleSubMenu(button) {
-    button.preventDefault();
-    console.log(button);
-
-    if (!button.target.nextElementSibling) return;
-
-    if (!button.target.nextElementSibling.classList.contains('show')) {
-      closeAllSubMenus()
-    }
-
-    button.target.nextElementSibling.classList.toggle('show')
-    button.target.classList.toggle('rotate')
-
-    if (sidebar.classList.contains('close')) {
-      sidebar.classList.toggle('close')
-      toggleButton.classList.toggle('rotate')
-    }
-  }
-
   function closeAllSubMenus() {
     Array.from(sidebar.getElementsByClassName('show')).forEach(ul => {
       ul.classList.remove('show')
+      console.log(ul.classList.remove('show'))
       ul.previousElementSibling.classList.remove('rotate')
     })
   }
@@ -60,12 +47,12 @@ export default function Navbar() {
           </Link>
         </li>
         <li>
-          <button onClick={(button) => toggleSubMenu(button)} className="dropdown-btn">
+          <button onClick={() => setIsShowCreate(!isShowCreate)} className="dropdown-btn">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M160-160q-33 0-56.5-23.5T80-240v-480q0-33 23.5-56.5T160-800h207q16 0 30.5 6t25.5 17l57 57h320q33 0 56.5 23.5T880-640v400q0 33-23.5 56.5T800-160H160Zm0-80h640v-400H447l-80-80H160v480Zm0 0v-480 480Zm400-160v40q0 17 11.5 28.5T600-320q17 0 28.5-11.5T640-360v-40h40q17 0 28.5-11.5T720-440q0-17-11.5-28.5T680-480h-40v-40q0-17-11.5-28.5T600-560q-17 0-28.5 11.5T560-520v40h-40q-17 0-28.5 11.5T480-440q0 17 11.5 28.5T520-400h40Z" /></svg>
             <span>Create</span>
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-361q-8 0-15-2.5t-13-8.5L268-556q-11-11-11-28t11-28q11-11 28-11t28 11l156 156 156-156q11-11 28-11t28 11q11 11 11 28t-11 28L508-372q-6 6-13 8.5t-15 2.5Z" /></svg>
           </button>
-          <ul className="sub-menu">
+          <ul className={isShowCreate ? "sub-menu show" : "sub-menu"}>
             <div>
               <li><a href="/#">Folder</a></li>
               <li><a href="/#">Document</a></li>
@@ -74,12 +61,12 @@ export default function Navbar() {
           </ul>
         </li>
         <li>
-          <button onClick={(button) => toggleSubMenu(button)} className="dropdown-btn">
+          <button onClick={() => setIsShowTodoList(!isShowTodoList)} className="dropdown-btn">
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="m221-313 142-142q12-12 28-11.5t28 12.5q11 12 11 28t-11 28L250-228q-12 12-28 12t-28-12l-86-86q-11-11-11-28t11-28q11-11 28-11t28 11l57 57Zm0-320 142-142q12-12 28-11.5t28 12.5q11 12 11 28t-11 28L250-548q-12 12-28 12t-28-12l-86-86q-11-11-11-28t11-28q11-11 28-11t28 11l57 57Zm339 353q-17 0-28.5-11.5T520-320q0-17 11.5-28.5T560-360h280q17 0 28.5 11.5T880-320q0 17-11.5 28.5T840-280H560Zm0-320q-17 0-28.5-11.5T520-640q0-17 11.5-28.5T560-680h280q17 0 28.5 11.5T880-640q0 17-11.5 28.5T840-600H560Z" /></svg>
             <span>Todo-Lists</span>
             <svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 -960 960 960" width="24px" fill="#e8eaed"><path d="M480-361q-8 0-15-2.5t-13-8.5L268-556q-11-11-11-28t11-28q11-11 28-11t28 11l156 156 156-156q11-11 28-11t28 11q11 11 11 28t-11 28L508-372q-6 6-13 8.5t-15 2.5Z" /></svg>
           </button>
-          <ul className="sub-menu">
+          <ul className={isShowTodoList ? "sub-menu show" : "sub-menu"}>
             <div>
               <li><a href="/#">Work</a></li>
               <li><a href="/#">Private</a></li>
